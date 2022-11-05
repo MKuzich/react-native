343b614f730da4c4620f0de0ae31f9c10eb2e321
Mykhailo Kuzich <timmkedth@gmail.com>
2022-11-05 23:27:40 +0200

    1

--- my-app/Screens/Home.jsx
+++ my-app/Screens/Home.jsx
@@ -1,3 +1,22 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableOpacity,
+  Dimensions,
+  TouchableWithoutFeedback,
+  Keyboard,
+  ImageBackground,
+} from "react-native";
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <View>
+        <Text>its home screen</Text>
+      </View>
+    </>
+  );
 }
